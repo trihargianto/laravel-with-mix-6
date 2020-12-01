355176c19efd7872b756e6f5abe7da19c344e72d
Tri Hargianto <trihargianto@gmail.com>
2020-12-01 15:21:00 +0700

    initial commit

---
+++ resources/js/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>Hello Im from Vue</div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  mounted() {
+    console.log('1.', process);
+    console.log('2.', process.env);
+    console.log('3.', process.env.TEST);
+    console.log('4.', process.env.MIX_TEST);
+  },
+};
+</script>
